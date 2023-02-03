b47aee69c01e59d214dcefd40024c0ce1c3d8b0d
Akira Watanabe <a0323w@gmail.com>
2023-02-03 13:04:07 +0900

    抽象クラスの問題が分からない...

--- ai_test.py
+++ ai_test.py
@@ -1,2 +1,40 @@
 '''「easyAI」使ってみる'''
-from easyAI import TwoPlayerGame, Human_Player, AI_Player
+from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
+
+class GameController(TwoPlayerGame):
+    '''ゲーム全体の管理'''
+    def __init__(self, player):
+        self.player = player
+        self.n_player = 1
+        self.board = [0] * 9
+
+    def possible_move(self):
+        '''指すことができるマスの列挙'''
+        return [a + 1 for a, b in enumerate(self.board) if b == 0]
+
+    def make_move(self, move):
+        '''盤面を更新する'''
+        self.board[int(move) - 1] = self.n_player
+
+    def loss_game(self):
+        '''負け判定'''
+        win_comb = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                    [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                    [1, 5, 9], [3, 5, 7]]
+        return any([all([(self.board[i-1] == self.opponent) for i in comb]) for comb in win_comb])
+
+    def is_over(self):
+        '''ゲームの終了判定'''
+        return (self.possible_move() == []) or self.loss_game()
+
+    def show_board(self):
+        '''盤面の表示'''
+        print('\n' + '\n'.join([' '.join([['.', 'o', 'x'][self.board[3*j + i]] for i in range(3)]) for j in range(3)]))
+
+    def scoring(self):
+        '''負けた場合に負けのスコアを返す'''
+        return -100 if self.loss_game() else 0
+
+algorithm = Negamax(7)
+game = GameController(player=[Human_Player(), AI_Player(algorithm)])
+game.play()
