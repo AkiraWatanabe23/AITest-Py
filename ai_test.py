e1621ebcf0dd312f4877da8fe2307bb67ba57ec9
Akira Watanabe <a0323w@gmail.com>
2023-02-04 10:36:54 +0900

    実行は出来るようになった（o, xの描画が不自然なので、修正する）

--- ai_test.py
+++ ai_test.py
@@ -3,12 +3,13 @@ from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
 
 class GameController(TwoPlayerGame):
     '''ゲーム全体の管理'''
-    def __init__(self, player):
-        self.player = player
-        self.n_player = 1
+    def __init__(self, players):
+        self.players = players
+        self.n_player = 2
+        self.current_player = 1
         self.board = [0] * 9
 
-    def possible_move(self):
+    def possible_moves(self):
         '''指すことができるマスの列挙'''
         return [a + 1 for a, b in enumerate(self.board) if b == 0]
 
@@ -25,16 +26,18 @@ class GameController(TwoPlayerGame):
 
     def is_over(self):
         '''ゲームの終了判定'''
-        return (self.possible_move() == []) or self.loss_game()
+        return (self.possible_moves() == []) or self.loss_game()
 
-    def show_board(self):
+    def show(self):
         '''盤面の表示'''
-        print('\n' + '\n'.join([' '.join([['.', 'o', 'x'][self.board[3*j + i]] for i in range(3)]) for j in range(3)]))
+        print('\n' + '\n'.join([' '.join([['.', 'o', 'x'][self.board[3*j + i]]
+                                          for i in range(3)]) for j in range(3)]))
 
     def scoring(self):
         '''負けた場合に負けのスコアを返す'''
         return -100 if self.loss_game() else 0
 
 algorithm = Negamax(7)
-game = GameController(player=[Human_Player(), AI_Player(algorithm)])
+#↓この部分でエラー出てる
+game = GameController([Human_Player(), AI_Player(algorithm)])
 game.play()
