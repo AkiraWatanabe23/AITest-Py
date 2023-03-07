2091fddf1c375d0d2e127312e306555cf63c6f73
Akira Watanabe <a0323w@gmail.com>
2023-03-07 22:35:36 +0900

    石の設置、反映（制限はこれからつける）

--- reversi.py
+++ reversi.py
@@ -9,7 +9,7 @@ WALL = 2
 BOARD_SIZE = 8
 
 class Board():
-    '''盤面の初期設定'''
+    '''盤面の設定'''
     def __init__(self):
         #↓つくっているものは同じだけど、出力の形式が異なる
         #self.board = [[0] * 10 for _ in range(10)]
@@ -27,5 +27,29 @@ class Board():
         self.board[4, 5] = BLACK
         self.board[5, 4] = BLACK
 
+        self.turn = 0
+        self.current_turn = WHITE
+
+    def set_stone(self, x_pos, y_pos):
+        '''石を配置する'''
+        if x_pos < 1 or BOARD_SIZE < x_pos:
+            return False
+        if y_pos < 1 or BOARD_SIZE < y_pos:
+            return False
+
+        self.flip_stone(x_pos, y_pos)
+        self.current_turn *= -1
+
+        return True
+
+    def flip_stone(self, x_pos, y_pos):
+        '''盤面の反映'''
+        self.board[x_pos, y_pos] = self.current_turn
+
 instance = Board()
-print(instance.board)
+
+for y in range(10):
+    for x in range(10):
+        #「^」中央揃えのformat指定子
+        print('{:^3}'.format(instance.board[x, y]), end="")
+    print()
