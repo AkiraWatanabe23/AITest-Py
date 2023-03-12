173c80136a43e638941b8777b02504a9985fae26
Akira Watanabe <a0323w@gmail.com>
2023-03-12 17:20:49 +0900

    戻り値の記述

--- ai_test.py
+++ ai_test.py
@@ -1,4 +1,4 @@
-'''「easyAI」使ってみる'''
+'''「easyAI」使ってみる(三目並べ)'''
 from easyAI import TwoPlayerGame, AI_Player, Negamax
 from easyAI.Player import Human_Player
 
@@ -10,23 +10,23 @@ class GameController(TwoPlayerGame):
         self.current_player = 1
         self.board = [0] * 9
 
-    def possible_moves(self):
-        '''指すことができるマスの列挙(return list[int])'''
+    def possible_moves(self) -> list[int]:
+        '''指すことができるマスの列挙'''
         return [a + 1 for a, b in enumerate(self.board) if b == 0]
 
     def make_move(self, move):
         '''盤面を更新する'''
         self.board[int(move) - 1] = self.nplayer
 
-    def loss_game(self):
-        '''負け判定(return bool)'''
+    def loss_game(self) -> bool:
+        '''負け判定'''
         win_comb = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
                     [1, 4, 7], [2, 5, 8], [3, 6, 9],
                     [1, 5, 9], [3, 5, 7]]
         return any([all([(self.board[i - 1] == self.opponent) for i in comb]) for comb in win_comb])
 
-    def is_over(self):
-        '''ゲームの終了判定(return bool)'''
+    def is_over(self) -> bool:
+        '''ゲームの終了判定'''
         return (self.possible_moves() == []) or self.loss_game()
 
     def show(self):
@@ -34,7 +34,7 @@ class GameController(TwoPlayerGame):
         print('\n' + '\n'.join([' '.join([['.', 'o', 'x'][self.board[3*j + i]]
                                           for i in range(3)]) for j in range(3)]))
 
-    def scoring(self):
+    def scoring(self) -> int:
         '''負けた場合に負けのスコアを返す'''
         return -100 if self.loss_game() else 0
 
